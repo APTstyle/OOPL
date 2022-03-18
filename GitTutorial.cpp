d34093c82195a5d0f6de99b93f4e179798caa42c
Chun-Ting <t108590026@ntut.edu.tw>
2022-03-18 10:24:56 +0800

    Signed-off-by: Chun-Ting <t108590026@ntut.edu.tw>

--- GitTutorial.cpp
+++ GitTutorial.cpp
@@ -3,13 +3,23 @@
 #include <cstdlib>
 #include <ctime>
 bool Guess(int number) {
- return false;
+static int target = -1;
+srand(time(NULL));
+if (target == -1) {
+target = rand() % 100 + 1;
+}
+if (number == target) { 
+std::cout << "Correct !!";
+target = -1;
+return true;
+}
+else std::cout << "Wrong" << std::endl; 
+return false;
 }
 int main()
 {
 int guess; 
 do {
-std::cout << "Choose a number between 1 - 100 :";
 std::cin >> guess;
 } while (!Guess(guess)); 
 return 0;
