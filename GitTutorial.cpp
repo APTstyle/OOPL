9c9ec97b1b22ffb0429b32b5f0cd5dfae5247a4f
Chun-Ting <t108590026@ntut.edu.tw>
2022-03-18 10:02:46 +0800

    Signed-off-by: Chun-Ting <t108590026@ntut.edu.tw>

--- GitTutorial.cpp
+++ GitTutorial.cpp
@@ -1,16 +1,12 @@
-#include <string> 
+#include <string>
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-bool Guess(int number) {
-	return false;
-}
-int main()
-{
-	int guess; 
-	do {
-		std::cout << "Choose a number between 1 - 100 :";
-		std::cin >> guess;
-	} while (!Guess(guess)); 
-	return 0;
+bool Guess(int number) { 
+return false; 
+} 
+int main() { 
+int guess;
+do {
+std::cin >> guess;
+} while (!Guess(guess));
+return 0;
 }
