6edd2721b1a91a154d9f603d7379b8a7c8f9ad72
Chun-Ting <t108590026@ntut.edu.tw>
2022-03-18 09:55:44 +0800

    Signed-off-by: Chun-Ting <t108590026@ntut.edu.tw>

---
+++ GitTutorial.cpp
@@ -0,0 +1,16 @@
+#include <string> 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+bool Guess(int number) {
+	return false;
+}
+int main()
+{
+	int guess; 
+	do {
+		std::cout << "Choose a number between 1 - 100 :";
+		std::cin >> guess;
+	} while (!Guess(guess)); 
+	return 0;
+}
