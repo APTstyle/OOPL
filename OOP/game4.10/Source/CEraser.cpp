c5af5b8d2237162653a47e557048ab1537518c00
“YuChun <ironman30832@gmail.com>
2022-04-08 02:46:42 +0800

    4/8 finish

---
+++ OOP/game4.10/Source/CEraser.cpp
@@ -0,0 +1,115 @@
+#include "stdafx.h"
+#include "Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "audio.h"
+#include "gamelib.h"
+#include "CEraser.h"
+
+namespace game_framework {
+	/////////////////////////////////////////////////////////////////////////////
+	// CEraser: Eraser class
+	/////////////////////////////////////////////////////////////////////////////
+
+	CEraser::CEraser()
+	{
+		Initialize();
+	}
+
+	int CEraser::Character() {
+		return character;
+	}
+
+	int CEraser::GetX1()
+	{
+		return x;
+	}
+
+	int CEraser::GetY1()
+	{
+		return y;
+	}
+
+	int CEraser::GetX2()
+	{
+		return x + animation.Width();
+	}
+
+	int CEraser::GetY2()
+	{
+		return y + animation.Height();
+	}
+
+	void CEraser::Initialize()
+	{
+		const int X_POS = 280;
+		const int Y_POS = 400;
+		character = 0;
+		x = X_POS;
+		y = Y_POS;
+		isMovingLeft = isMovingRight = isMovingUp = isMovingDown = false;
+	}
+
+	void CEraser::LoadBitmap()
+	{
+		animation.AddBitmap(warrior, RGB(255, 255, 255));
+		/*if (character == 1) {
+			animation.AddBitmap(warrior, RGB(255, 255, 255));
+		}
+		else if (character == 2) {
+			animation.AddBitmap(mage, RGB(255, 255, 255));
+		}
+		else if (character == 3) {
+			animation.AddBitmap(assassin, RGB(255, 255, 255));
+		}
+		else if (character == 4) {
+			animation.AddBitmap(hunter, RGB(255, 255, 255));
+		}*/
+		
+	}
+
+	void CEraser::OnMove()
+	{
+		const int STEP_SIZE = 10;
+		animation.OnMove();
+		if (isMovingLeft)
+			x -= STEP_SIZE;
+		if (isMovingRight)
+			x += STEP_SIZE;
+		if (isMovingUp)
+			y -= STEP_SIZE;
+		if (isMovingDown)
+			y += STEP_SIZE;
+	}
+
+	void CEraser::SetMovingDown(bool flag)
+	{
+		isMovingDown = flag;
+	}
+
+	void CEraser::SetMovingLeft(bool flag)
+	{
+		isMovingLeft = flag;
+	}
+
+	void CEraser::SetMovingRight(bool flag)
+	{
+		isMovingRight = flag;
+	}
+
+	void CEraser::SetMovingUp(bool flag)
+	{
+		isMovingUp = flag;
+	}
+
+	void CEraser::SetXY(int nx, int ny)
+	{
+		x = nx; y = ny;
+	}
+
+	void CEraser::OnShow()
+	{
+		animation.SetTopLeft(x, y);
+		animation.OnShow();
+	}
+}
